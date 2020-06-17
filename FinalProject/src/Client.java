834d3392e2720d55cffb1b2c0ddab8b98a4ceeb9
Panos <Panos@192.168.71.1>
2020-06-17 16:48:37 +0300

    Prwtes Klaseis

---
+++ FinalProject/src/Client.java
@@ -0,0 +1,48 @@
+
+public class Client {
+
+	private int id_number;
+	private int license_number;
+	private String name;
+	private String email;
+	private String address;
+	private int telephone;
+	
+	public int getId_number() {
+		return id_number;
+	}
+	public void setId_number(int id_number) {
+		this.id_number = id_number;
+	}
+	public int getLicense_number() {
+		return license_number;
+	}
+	public void setLicense_number(int license_number) {
+		this.license_number = license_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+}
---
+++ FinalProject/src/Rental.java
@@ -0,0 +1,76 @@
+
+public class Rental {
+
+	private int res_Code;
+	private String vehicle;
+	private String customer;
+	private String branch;
+	private int pick_Time;
+	private String pick_Date;
+	private String ret_Branch;
+	private int ret_Time;
+	private String ret_Date;
+	private int total_Cost;
+	
+	public int getRes_Code() {
+		return res_Code;
+	}
+	public void setRes_Code(int res_Code) {
+		this.res_Code = res_Code;
+	}
+	public String getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getPick_Time() {
+		return pick_Time;
+	}
+	public void setPick_Time(int pick_Time) {
+		this.pick_Time = pick_Time;
+	}
+	public String getPick_Date() {
+		return pick_Date;
+	}
+	public void setPick_Date(String pick_Date) {
+		this.pick_Date = pick_Date;
+	}
+	public String getRet_Branch() {
+		return ret_Branch;
+	}
+	public void setRet_Branch(String ret_Branch) {
+		this.ret_Branch = ret_Branch;
+	}
+	public int getRet_Time() {
+		return ret_Time;
+	}
+	public void setRet_Time(int ret_Time) {
+		this.ret_Time = ret_Time;
+	}
+	public String getRet_Date() {
+		return ret_Date;
+	}
+	public void setRet_Date(String ret_Date) {
+		this.ret_Date = ret_Date;
+	}
+	public int getTotal_Cost() {
+		return total_Cost;
+	}
+	public void setTotal_Cost(int total_Cost) {
+		this.total_Cost = total_Cost;
+	}
+	
+}
